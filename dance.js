9d656057e07029145aebad0203da790fd8269c42
Nouf Aljowaysir <na1215@nyu.edu>
2018-04-07 01:16:30 -0400

    updating

--- dance.js
+++ dance.js
@@ -10,8 +10,10 @@
 let container, renderer, camera, scene;
 let button, action;
 let clicked = false;
+let prev_clicked = false;
 let controls, loader, effect;
 let pointLight, pointLight2;
+let light_sources = [];
 let circles = [];
 let mixers = [];
 let clock = new THREE.Clock();
@@ -29,6 +31,7 @@ function onLoad(){
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(wid, hei);
 	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.autoUpdate = true;
 	container.appendChild(renderer.domElement);
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0x222222 );
@@ -37,18 +40,22 @@ function onLoad(){
 	// dollyCam.add(camera);
 	// scene.add(dollyCam);
 
-	camera.position.set(0, 0, 230);
-	//camera.lookAt(new THREE.Vector3(0,0,50));
-	//camera.target.set(0,0,0);
-
 	controls = new THREE.OrbitControls( camera );
 	controls.target.set( 0, 0, 0 );
-	controls.minDistance = 0;
-	controls.maxDistance = 400;
-	controls.minPolarAngle = 0; // radians
-  	controls.maxPolarAngle = Math.PI; // radians
-	controls.zoomSpeed = 2.0;
 	controls.update();
+
+	// camera.position.set(-86.95797110098906,   -3.571536128474456,   139.4166129600535);
+	// camera.rotation.set( -0.2160542551663327, -0.35443077885459445,  -0.0760251675039741);
+	camera.focus = 10;
+	// camera.matrix.fromArray(cam_array);
+	// camera.matrix.decompose(camera.position, camera.quaternion, camera.scale);
+	// camera.quaternion.set(-0.09026617734075532, -0.2400304459020302, -0.022422273552483593, 0.9662994587171708);
+	camera.position.set(-100.31179766825836, -34.70840403385185,  78.839936443855);
+	camera.rotation.set(-0.1961224731182619, -0.5183480659355434, -0.09811729163485307);
+	camera.updateProjectionMatrix();
+	//camera.lookAt(new THREE.Vector3(0,0,50));
+
+
 
 	createEnvironment();
 	button = document.getElementById("dance");
@@ -56,76 +63,82 @@ function onLoad(){
 	window.addEventListener('resize', onWindowResize, true );
 }
 
+
+
+
 // ENVIRONMENT
 function createEnvironment(){
 	// Room
 	let geometry = new THREE.BoxGeometry( 400, 400, 800 );
 	let boxMaterial = new THREE.MeshPhongMaterial( {
-	color: 0xa0adaf,
-	shininess: 10,
-	specular: 0x111111,
-	side: THREE.DoubleSide
-	} );
+					color: 0xa0adaf,
+					// shininess: 10,
+					specular: 0x111111,
+					side: THREE.BackSide
+				} );
 	let cube = new THREE.Mesh( geometry, boxMaterial );
 	cube.receiveShadow = true;
 	cube.position.set(0,0,0);
 	scene.add( cube );
 
-	// Objects in scene
-	let coneGeo = new THREE.ConeGeometry( 100, 300, 300 );
-	let conMaterial = new THREE.MeshPhongMaterial( {color: 0xffffff} );
-	let cone = new THREE.Mesh( coneGeo, conMaterial );
-	cone.position.set(0,0,-380);
-	//scene.add( cone );
+	let plane_geo = new THREE.PlaneGeometry(400, 1000, 2, 2);
+	let plane_mat = new THREE.MeshPhongMaterial( {
+					color: 0xa0adaf,
+					// shininess: 10,
+					specular: 0x111111,
+					side: THREE.DoubleSide
+				} );
+	let plane = new THREE.Mesh(plane_geo, plane_mat);
+	plane.receiveShadow = true;
+	plane.rotation.x = Math.PI/2;
+	plane.position.y = -200;
+	// scene.add(plane);
 
-	let circle = new THREE.SphereGeometry( 50, 50, 50 );
-	let circMaterial = new THREE.MeshPhongMaterial( {color: 0xffffff} );
+	let wall1 = new THREE.Mesh(plane_geo, plane_mat);
+	wall1.receiveShadow = true;
+	wall1.rotation.y = Math.PI/2;
+	wall1.rotation.z = Math.PI/2;
+	wall1.position.set(-200, 0, 0);
+	// scene.add(wall1);
 
-	for (let r=1;r<10;r++) {
-		let circleMesh = new THREE.Mesh( circle, circMaterial );
-		circles.push(circleMesh);
-		circleMesh.position.set(THREE.Math.randInt(-400,400), -450, THREE.Math.randInt(-400,100));
-		circleMesh.castShadow = true;
-		circleMesh.receiveShadow = false;
-		scene.add( circleMesh );
-	}
-	//circleMesh.position.set(-400,-450,-300);
-
-	//GORILLA
+	let wall2 = new THREE.Mesh(plane_geo, plane_mat);
+	wall2.receiveShadow = true;
+	wall2.rotation.y = Math.PI/2;
+	wall2.rotation.z = Math.PI/2;
+	wall2.position.set( 200, 0, 0);
+	// scene.add(wall2);
 
 	// model
 	var manager = new THREE.LoadingManager();
 	var textureLoader = new THREE.TextureLoader( manager );
 	var texture = textureLoader.load( 'models/Nouf1_texture.jpg' );
 	var loader = new THREE.FBXLoader();
-	loader.load( 'models/Samba Dancing.fbx', function ( object ) {
+	loader.load( 'models/Samba_Dancing.fbx', function ( object ) {
 		object.mixer = new THREE.AnimationMixer( object );
 		mixers.push( object.mixer );
 		action = object.mixer.clipAction( object.animations[ 0 ] );
 		action.play();
 		object.traverse( function ( child ) {
-		if ( child.isMesh ) {
-			//child.material.map = texture;
-			child.castShadow = true;
-			child.receiveShadow = true;
-		}
-	});
-	scene.add( object );
-	//object.scale.set(6, 6, 6)
-	object.position.set(0,-200,-150);
+			if ( child.isMesh ) {
+				//child.material.map = texture;
+				child.castShadow = true;
+				child.receiveShadow = true;
+			}
+		} );
+		scene.add( object );
+		//object.scale.set(6, 6, 6)
+	  object.position.set(0,-200,-100);
 	} );
+	console.log(loader);
 
 	// LIGHTS!
-	let ambient = new THREE.AmbientLight( 0x3BAB60,0.5);
+	let ambient = new THREE.AmbientLight( 0x3BAB60, 0.1);
 	ambient.castShadow = true;
-	scene.add( ambient );
-	// let d_light = new THREE.DirectionalLight(0xffffff, 1);
-	// d_light.position =
-	// scene.add(d_light);
+	// scene.add( ambient );
 
-	pointLight = new THREE.PointLight(0xff0000, 2, 1000, 1);
+	pointLight = new THREE.PointLight(0xff0000, 2, 500, 1);
 	pointLight.position.set(0, 0, 0);
-	let lightGeo1 = new THREE.SphereGeometry( 0.5, 50, 50 );
+	let lightGeo1 = new THREE.SphereGeometry( 0.5, 12, 12 );
 	let lightMat1 = new THREE.MeshPhongMaterial( {
 		side: THREE.DoubleSide,
 		alphaTest: 0.5
@@ -133,14 +146,15 @@ function createEnvironment(){
 	let lightMesh1 = new THREE.Mesh( lightGeo1, lightMat1 );
 	lightMesh1.castShadow = true;
 	lightMesh1.receiveShadow = true;
-	//pointLight.add( lightMesh1 );
+	// pointLight.add( lightMesh1 );
 	pointLight.castShadow = true;
-
+	light_sources.push(pointLight);
 	scene.add(pointLight);
 
-	pointLight2 = new THREE.PointLight(0x0000D0, 2, 1000, 1);
+
+	pointLight2 = new THREE.PointLight(0x0000D0, 2, 500, 1);
 	pointLight2.position.set(0, 0, 0);
-	let lightGeo2 = new THREE.SphereGeometry( 0.5, 50, 50 );
+	let lightGeo2 = new THREE.SphereGeometry( 0.5, 12, 12 );
 	let lightMat2 = new THREE.MeshPhongMaterial( {
 		side: THREE.DoubleSide,
 		alphaTest: 0.5
@@ -148,11 +162,10 @@ function createEnvironment(){
 	let lightMesh2 = new THREE.Mesh( lightGeo2, lightMat2 );
 	lightMesh2.castShadow = true;
 	lightMesh2.receiveShadow = true;
-	//pointLight2.add( lightMesh2 );
+	// pointLight2.add( lightMesh2 );
 	pointLight2.castShadow = true;
-
 	scene.add(pointLight2);
-
+	light_sources.push(pointLight2);
 }
 
 function dancing() {
@@ -170,18 +183,11 @@ function onWindowResize(){
   let wid = window.innerWidth;
   let hei = window.innerHeight;
 
-	//effect.setSize(wid, hei);
-  //renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(wid, hei);
 	camera.aspect = wid/hei;
   camera.updateProjectionMatrix();
 }
 
-
-// ANIMATION
-// function update(){
-// 	window.requestAnimationFrame(animate);
-// }
 
 function animate() {
 
@@ -190,33 +196,36 @@ function animate() {
 
 	if (clicked == false)
 	{
+		if( prev_clicked == true ) prev_clicked = false;
 		//stay still
 	} else {
+		if( prev_clicked == false ){
+			prev_clicked = true;
+			clock.getDelta();
+		}
 		if ( mixers.length > 0 ) {
 			for ( var i = 0; i < mixers.length; i ++ ) {
 				mixers[ i ].update( clock.getDelta() );
 			}
 		}
 	}
-	// if ( mixers.length > 0 ) {
-	// 	for ( var i = 0; i < mixers.length; i ++ ) {
-	// 		mixers[ i ].update( clock.getDelta() );
-	// 	}
-	// }
 
-	pointLight.position.x = Math.sin( time * 0.6 ) * 400;
-	pointLight.position.y = Math.sin( time * 0.7 ) * 400 + 5;
-	pointLight.position.z = Math.sin( time * 0.8 ) * 400;
-	pointLight.rotation.x = time;
-	pointLight.rotation.z = time;
+
+	pointLight.position.x = Math.sin( time * 0.6 ) * 100;
+	pointLight.position.y = Math.sin( time * 0.7 ) * 100 + 50;
+	pointLight.position.z = Math.sin( time * 0.8 ) * 200;
 	time += 10000;
+	pointLight.updateMatrix();
+	pointLight.updateMatrixWorld();
 
-	// pointLight2.position.x = Math.sin( time * 0.6 ) * 200;
-	// pointLight2.position.y = Math.sin( time * 0.7 ) * 200 + 5;
-	// pointLight2.position.z = Math.sin( time * 0.8 ) * 200;
-	// pointLight2.rotation.x = time;
-	// pointLight2.rotation.z = time;
+	pointLight2.position.x = Math.cos( time * 0.6 ) * 100;
+	pointLight2.position.y = Math.cos( time * 0.7 ) * 100 + 50;
+	pointLight2.position.z = Math.cos( time * 0.8 ) * 200;
+	pointLight2.updateMatrix();
+	pointLight2.updateMatrixWorld();
 
-	//controls.update();
+
+	// controls.update();
+	renderer.shadowMap.needsUpdate = true;
 	renderer.render(scene, camera);
 }
